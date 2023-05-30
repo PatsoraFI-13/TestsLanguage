76356ad80e2c513be54c1cf2d9e8cd13b9b6c0fe
PatsoraFI-13 <125893837+PatsoraFI-13@users.noreply.github.com>
2023-05-30 19:01:47 +0200

    3.

--- TestsLanguage/Tests/GarbageCollectorTests.cs
+++ TestsLanguage/Tests/GarbageCollectorTests.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 
 [TestFixture]
 public class GarbageCollectorTests
@@ -6,17 +6,22 @@ public class GarbageCollectorTests
     [Test]
     public void MemoryAllocation_ExceedingLimit()
     {
-        // Створення об'єктів до тих пір, поки не вийде за межі доступної пам'яті
-        while (true)
+        try
         {
-            var obj = new byte[1024 * 1024]; // Виділення 1 МБ пам'яті
+            // Створення об'єктів до тих пір, поки не вийде за межі доступної пам'яті
+            while (true)
+            {
+                var obj = new byte[1024 * 1024]; // Виділення 1 МБ пам'яті
 
-            // Перевірка, чи відбулося очищення пам'яті
-            if (obj == null)
-                break;
+                // Перевірка, чи відбулося очищення пам'яті
+                if (obj == null)
+                    break;
+            }
         }
-
-        // Успішно очищено пам'ять, якщо код досяг цієї точки
-        Assert.Pass();
+        catch (OutOfMemoryException)
+        {
+            Assert.Pass();
+        }
+        Assert.Fail();
     }
 }
