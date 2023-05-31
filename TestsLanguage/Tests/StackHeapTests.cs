a219bc61ca029b36abc86b338a0d192e801c7747
PatsoraFI-13 <125893837+PatsoraFI-13@users.noreply.github.com>
2023-05-31 20:06:05 +0200

    Update StackHeapTests.cs

--- TestsLanguage/Tests/StackHeapTests.cs
+++ TestsLanguage/Tests/StackHeapTests.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 
 public struct ExampleStruct
 {
@@ -11,21 +11,15 @@ public class StackHeapTests
     [Test]
     public void StackAllocation()
     {
-        ExampleStruct stackObj = new ExampleStruct();
-        stackObj.Value = 10;
-
-        Assert.AreEqual(10, stackObj.Value);
+        var obj = new ExampleStruct();
+        Assert.IsFalse(obj.GetType().IsValueType == false);
     }
 
     [Test]
     public void HeapAllocation()
     {
-        SimpleClass heapObj = new SimpleClass();
-        heapObj.Value = 10;
-
-        SimpleClass heapObj2 = heapObj;
-        heapObj2.Value = 20;
-
-        Assert.AreNotEqual(heapObj.Value, heapObj2.Value);
+        SimpleClass obj = new SimpleClass();
+        Assert.IsTrue(obj.GetType().IsValueType == false);
     }
+
 }
